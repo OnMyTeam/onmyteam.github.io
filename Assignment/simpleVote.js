0367f2ed022eeeb5cf9b1f514d5b007d9a61c3a0
sanggi <sangiki07@gmail.com>
2019-08-30 15:49:03 +0900

    Assignment

--- Assignment/simpleVote.js
+++ Assignment/simpleVote.js
@@ -165,8 +165,8 @@ window.addEventListener('load', function() {
   } else {
     console.log('No web3? You should consider trying MetaMask!')
 	// fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-	
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+	web3js = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
   }	
   // Now you can start your app & access web3 freely:
   startApp();
